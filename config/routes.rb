72238fa2ed13826a075cd62b3d8343bbfe62f557
Sarah <smarmale@gmail.com>
2012-01-15 21:11:39 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
